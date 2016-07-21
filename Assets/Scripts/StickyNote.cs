23b9d6848176643edfebb7df76eb3f932b128295
sumarsky <i.dimcevski@gmail.com>
2016-07-21 13:18:30 +0200

    initial infinite sticky notes enabled

---
+++ Assets/Scripts/StickyNote.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(DuplicateManager))]
+[RequireComponent(typeof(TapToPlaceParent))]
+public class StickyNote : MonoBehaviour
+{
+    DuplicateManager _duplicateManager;
+    TapToPlaceParent _tapToPlaceParent;
+
+    void Start()
+    {
+        _duplicateManager = GetComponent<DuplicateManager>();
+        _tapToPlaceParent = GetComponent<TapToPlaceParent>();
+    }
+
+    public void Duplicate()
+    {
+        _duplicateManager.Duplicate();
+    }
+    public void PlaceParent()
+    {
+        _tapToPlaceParent.OnSelect();
+    }
+}
