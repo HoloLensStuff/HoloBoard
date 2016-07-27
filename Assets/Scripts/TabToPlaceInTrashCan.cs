d4619ea919bb65876b05c339286dfceee505b288
Eljesa Zekjiri <ezekjiri@endava.net>
2016-07-27 11:03:00 +0200

    added other colors for sticky notes, and a recycle bin for deleteing the sticky notes

---
+++ Assets/Scripts/TabToPlaceInTrashCan.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabToPlaceInTrashCan : MonoBehaviour {
+
+    public Material defaultMaterial;
+    public Material activeObjectMaterial;
+
+    Renderer currentRenderer;
+    bool placing = false;
+
+    void Start()
+    {
+        currentRenderer = GetComponent<Renderer>();
+    }
+
+    public bool IsPlacingMode() { return placing; }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    public void OnSelect()
+    {
+        placing = !placing;
+
+        currentRenderer.material = placing ? activeObjectMaterial : defaultMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (placing)
+        {
+            int trasCanLayer = 1 << 9;
+
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+                30.0f, trasCanLayer))
+            {
+                this.transform.position = hitInfo.point;
+
+                Quaternion toQuat = Camera.main.transform.localRotation;
+                toQuat.x = 0;
+                toQuat.z = 0;
+                this.transform.rotation = toQuat;
+            }
+        }
+    }
+}
