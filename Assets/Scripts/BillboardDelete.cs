79ba8b1efb901069c182bbb180a7d3d12f8793b4
sumarsky <i.dimcevski@gmail.com>
2016-08-09 14:13:41 +0200

    TagAlong prefab Edit / Delete

---
+++ Assets/Scripts/BillboardDelete.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BillboardDelete : MonoBehaviour
+{
+    public void Delete()
+    {
+        TagAlong tagAlong = FindObjectOfType<TagAlong>();
+        if (tagAlong == null)
+            return;
+
+        Destroy(tagAlong.ObjectToDelete);
+        Destroy(tagAlong.gameObject);
+    }
+}
